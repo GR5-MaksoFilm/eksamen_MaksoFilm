003ea64e9dd0b2e1e03bd222c2712ae73931680c
multimediemartin <maso0013@stud.kea.dk>
2025-06-01 16:09:26 +0200

    Refactor burger.js to initialize event listeners within DOMContentLoaded and remove unused menuClick function

--- src/assets/js/burger.js
+++ src/assets/js/burger.js
@@ -1,14 +1,12 @@
-const burger = document.querySelector("#burger");
-const nav = document.querySelector("nav");
-var menu = document.querySelectorAll(".menu");
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  const burger = document.querySelector("#burger");
+  const nav = document.querySelector("nav");
 
 burger.addEventListener("click", burgerClick);
 function burgerClick() {
   burger.classList.toggle("active");
   nav.classList.toggle("active");
 }
-menu.addEventListener("click", menuClick);
-function menuClick() {
-  burger.classList.remove("active");
-  nav.classList.remove("active");
-}
+});
